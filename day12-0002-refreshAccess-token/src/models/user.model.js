54e9155fadd03c538c9c12839d2374d46f5f4a0b
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-09-08 22:02:12 +0530

    start of access and refresh token

---
+++ day12-0002-refreshAccess-token/src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
